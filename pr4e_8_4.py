ec6b1794118b0c97d1886a9af30235c0181c359b
Ed Fraser <edfraser@yahoo.com>
2014-07-29 23:47:36 -0700

    code clean

--- pr4e_8_4.py
+++ pr4e_8_4.py
@@ -1,21 +1,19 @@
 fname = raw_input('Enter file name: ')
 if len(fname) < 1 : fname = 'romeo.txt'
     
-try:
-    fh = open(fname) 
-
-except:
+try: fh = open(fname) 
+except: 
     print 'File not found: ', fname
-    sys.exit()
+    exit()
 
 lst = list()
 unique_lst = list()
 
 for line in fh:
     lst = line.split()
-    for i in range(len(lst)):
-        if lst[i] in unique_lst: continue
-        unique_lst.append(lst[i])
+    for word in range(len(lst)):
+        if lst[word] in unique_lst: continue
+        unique_lst.append(lst[word])
         
 unique_lst.sort()
 print unique_lst
