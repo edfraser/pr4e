35bc4e44eac905a56994e6f0bfcfb2865921811f
Ed Fraser <edfraser@yahoo.com>
2014-08-01 14:44:46 -0700

    astrisk graph of distribution

--- pr4e_10_2.py
+++ pr4e_10_2.py
@@ -1,4 +1,4 @@
-# Print out the distribution by hour of the day for each email message in a text file
+# Create a distribution: number of emails sent each hour
 
 name = raw_input('Enter file:')
 if len(name) < 1 : name = 'mbox-short.txt'
@@ -9,18 +9,19 @@ except:
 
 count = {}
 for line in fh:
+    # parse line with email out of text file
     if not line.startswith('From '): continue
     
     # parse hour out of string
     atpos = line.find(':') - 2
     hour = line[atpos : atpos+2]
     
-    # count occurances of each hour
+    # count occurrences of each hour
     count[hour] = count.get(hour,0) + 1
     
 # make list of tuples, sort on key    
 lst = count.items()
 lst.sort()
 
-# print keys, values 
-for key, value in lst: print key, value
+# print distribution using an asterisk for each email occurrence / hour
+for key, value in lst: print key, value, '*' * value
